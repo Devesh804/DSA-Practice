55cb0ced5252ce5856913505c5c0e047b011fa9c
Devesh804 <deveshc280@gmail.com>
2026-09-13 10:22:58 +0530

    ShiftBy1

--- Array_manipulation/array_manipulationProblems.java
+++ Array_manipulation/array_manipulationProblems.java
@@ -1,4 +1,6 @@
 public class array_manipulationProblems {
+
+    //REverse an array
     static void reversearray(int arr[]){
         int n = arr.length;
         int i = 0;
@@ -16,11 +18,32 @@ public class array_manipulationProblems {
         for(int k:arr){
             System.out.println(k);
         }
+        /* Main file
+         public static void main(String[] args) {
+        int arr[] = {1,2,3,4,6};
+        reversearray(arr);
+    }
+
+} */
+    }
+
+    //ShiftBy1
+    static void shiftby1(int[] arr) {
+        int n=arr.length-1;
+        int temp = arr[n];
+        for(int i=n; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,6};
-        reversearray(arr);
+        int arr[] = {1,2,3,4,5,6};
+        shiftby1(arr);
+        for(int num:arr){
+            System.out.println(num + " ");
+        }
+        System.out.println();
     }
 
 }
